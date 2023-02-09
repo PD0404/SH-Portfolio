a59ccf1eda6fdc92472fa0227e1f586af049d918
Pranjal Das <pranjal04das@gmail.com>
2023-02-09 23:13:52 +0530

    Profile Updated

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$to = "sabirnits15@gmail.com";
+// Get data from form 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$headers = "From:" . $email;
+$txt = "From:" . $name . "Message:" . $message;
+if(mail($to,$subject,$message,$headers)) {
+    echo "Message sent successfully.";
+} else {
+    echo "Message was not sent. Retry after some time";
+}
+?>
